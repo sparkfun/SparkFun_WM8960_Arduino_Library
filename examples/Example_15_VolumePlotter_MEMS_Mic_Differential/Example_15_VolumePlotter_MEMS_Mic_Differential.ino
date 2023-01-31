27dc9aac9815e91c75a745508f1f666e2fa2f2b0
Pete Lewis <601236+lewispg228@users.noreply.github.com>
2023-01-31 16:36:14 -0700

    Example_15_VolumePlotter_MEMS_Mic_Differential
    This example uses a differential mic signal from the MEMS VM2020 mic, and plots the amplitude of the left channel to the serial plotter.

---
+++ examples/Example_15_VolumePlotter_MEMS_Mic_Differential/Example_15_VolumePlotter_MEMS_Mic_Differential.ino
@@ -0,0 +1,303 @@
+/******************************************************************************
+  Example_15_VolumePlotter_MEMS_Mic_Differential.ino
+  This example is very similar to example 11, however it uses a MEMS mic with
+  differential signal as the source of the sound. Here we are using just one of
+  VM2020 Breakouts plugged into the left channel.
+
+  Note, with this VM2020 differential mic signal, we are going to add much more
+  gain than in previous examples. We are going to use a MICBOOST of 29dB (max),
+  and set the PGA to +8.25dB.
+  
+  Demonstrates reading I2S audio from the ADC, and plotting the audio samples on 
+  the Arduino Serial Plotter.
+  
+  This example sets up differential analog audio input (on LINPUT1/LINPUT2), 
+  ADC enabled as I2S peripheral, sets volume control, and Headphone output on 
+  the WM8960 Codec.
+
+  A MEMS mic should be connected to the left channel INPUT1 and INPUT2, they are
+  labeled "LIN1" and "LIN2" on the board. These will provide the input 
+  connections for the positive and negative signals from the MEMS mic.
+
+  This example will pass your audio source through the mixers and gain stages of 
+  the codec into the ADC. Read the audio from the ADC via I2S.
+
+  The analog bypass paths is also setup, so your audio will pass through the 
+  codec and playback on HP outs.
+
+  Development platform used:
+  SparkFun ESP32 IoT RedBoard v10
+
+  HARDWARE CONNECTIONS
+
+  **********************
+  ESP32 ------- CODEC
+  **********************
+  QWIIC ------- QWIIC       *Note this connects GND/3.3V/SDA/SCL
+  GND --------- GND         *optional, but not a bad idea
+  5V ---------- VIN         *needed to power codec's onboard AVDD (3.3V vreg)
+  16 ---------- BCK         *aka BCLK/I2S_SCK/"bit clock", this is the clock for I2S audio, can be controlled via controller or peripheral.
+  17 ---------- ADAT        *aka ADC_DATA/I2S_SD/"serial data in", this carries the I2S audio data from codec's ADC to ESP32 I2S bus.
+  25 ---------- ALR        *aka I2S_WS/LRC/"word select"/"left-right-channel", this toggles for left or right channel data.
+
+  **********************
+  CODEC ------- MIC IN
+  **********************
+  GND --------- GND        *Ground
+  AVDD -------- VCC        *3.3V (default on the Codec breakout)
+  LINPUT1 ----- OUT-       *Mic signal "+"
+  LINPUT2 ----- OUT+       *Mic signal "-"
+
+  **********************
+  CODEC -------- AUDIO OUT
+  **********************
+  OUT3 --------- TRS OUTPUT SLEEVE          *buffered "vmid" (aka "HP GND")
+  HPL ---------- TRS OUTPUT TIP             *left HP output
+  HPR ---------- TRS OUTPUT RING1           *right HP output
+
+  You can now control the volume of the codecs built in headphone amp using this 
+  fuction:
+
+  codec.setHeadphoneVolumeDB(6.00); Valid inputs are -74.00 (MUTE) up to +6.00, 
+  (1.00dB steps).
+
+  Pete Lewis @ SparkFun Electronics
+  October 14th, 2022
+  https://github.com/sparkfun/SparkFun_WM8960_Arduino_Library
+  
+  This code was created using some code by Mike Grusin at SparkFun Electronics
+  Included with the LilyPad MP3 example code found here:
+  Revision history: version 1.0 2012/07/24 MDG Initial release
+  https://github.com/sparkfun/LilyPad_MP3_Player
+
+  This code was created using some modified code from DroneBot Workshop.
+  Specifically, the I2S configuration setup was super helpful to get I2S working.
+  This example has a similar I2S config to what we are using here: Microphone to 
+  serial plotter example. Although, here we are doing a line level TRS audio 
+  input into the codec. To see the original Drone Workshop code and learn more 
+  about I2S in general, please visit: https://dronebotworkshop.com/esp32-i2s/
+
+  Do you like this library? Help support SparkFun. Buy a board!
+
+    SparkFun Audio Codec Breakout - WM8960 (QWIIC)
+    https://www.sparkfun.com/products/21250
+
+    SparkFun Analog MEMS Microphone Breakout - VM2020
+    https://www.sparkfun.com/products/21537
+	
+	All functions return 1 if the read/write was successful, and 0
+	if there was a communications failure. You can ignore the return value
+	if you just don't care anymore.
+
+	For information on the data sent to and received from the CODEC,
+	refer to the WM8960 datasheet at:
+	https://github.com/sparkfun/SparkFun_Audio_Codec_Breakout_WM8960/blob/main/Documents/WM8960_datasheet_v4.2.pdf
+  This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+  Please review the LICENSE.md file included with this example. If you have any questions 
+  or concerns with licensing, please contact techsupport@sparkfun.com.
+  Distributed as-is; no warranty is given.
+******************************************************************************/
+
+#include <Wire.h>
+#include <SparkFun_WM8960_Arduino_Library.h> 
+// Click here to get the library: http://librarymanager/All#SparkFun_WM8960
+WM8960 codec;
+
+// Include I2S driver
+#include <driver/i2s.h>
+
+// Connections to I2S
+#define I2S_WS 25
+#define I2S_SD 17
+#define I2S_SDO 4 // Not used for this example
+#define I2S_SCK 16
+
+// Use I2S Processor 0
+#define I2S_PORT I2S_NUM_0
+
+// Define input buffer length
+#define bufferLen 64
+int16_t sBuffer[bufferLen];
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  if (codec.begin() == false) //Begin communication over I2C
+  {
+    Serial.println("The device did not respond. Please check wiring.");
+    while (1); // Freeze
+  }
+
+  codec_setup();
+
+  // Set up I2S
+  i2s_install();
+  i2s_setpin();
+  i2s_start(I2S_PORT);
+}
+
+void loop()
+{
+
+  // False print statements to "lock range" on serial plotter display
+  // Change rangelimit value to adjust "sensitivity"
+  int rangelimit = 3000;
+  Serial.print(rangelimit * -1);
+  Serial.print(" ");
+  Serial.print(rangelimit);
+  Serial.print(" ");
+
+  // Get I2S data and place in data buffer
+  size_t bytesIn = 0;
+  size_t bytesOut = 0;
+  esp_err_t result = i2s_read(I2S_PORT, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
+
+  if (result == ESP_OK)
+  {
+    // Read I2S data buffer
+    int16_t samples_read = bytesIn / 8;
+    if (samples_read > 0) {
+      float mean = 0;
+      // Only looking at left signal samples in the buffer (e.g. 0,2,4,6,8...)
+      // Notice in our for loop here, we are incrementing the index by 2.
+      for (int16_t i = 0; i < samples_read; i += 2) {
+        mean += (sBuffer[i]);
+      }
+
+      // Average the data reading
+      // We're only concerned with left input for this example. So we must
+      // divide by "half of samples read" (because it is stereo I2S audio data)
+      mean /= (samples_read / 2); 
+
+      // Print to serial plotter
+      Serial.println(mean);
+    }
+  }
+}
+
+void codec_setup()
+{
+  // General setup needed
+  codec.enableVREF();
+  codec.enableVMID();
+
+  // Setup signal flow to the ADC
+
+  codec.enableLMIC();
+  codec.enableRMIC();
+
+  // Connect from INPUT1 to "n" (aka inverting) inputs of PGAs.
+  codec.connectLMN1();
+  codec.connectRMN1();
+
+  // Disable mutes on PGA inputs (aka INTPUT1)
+  codec.disableLINMUTE();
+  codec.disableRINMUTE();
+
+  // Set pga volumes
+  codec.setLINVOLDB(8.25); // Valid options are -17.25dB to +30dB (0.75dB steps)
+  codec.setRINVOLDB(8.25); // Valid options are -17.25dB to +30dB (0.75dB steps)
+
+  // Set input boosts to get inputs 1 to the boost mixers
+  codec.setLMICBOOST(WM8960_MIC_BOOST_GAIN_29DB);
+  codec.setRMICBOOST(WM8960_MIC_BOOST_GAIN_29DB);
+
+    // For MIC+ signal of differential mic signal
+  codec.pgaLeftNonInvSignalSelect(WM8960_PGAL_LINPUT2); 
+
+  // For MIC+ signal of differential mic signal
+  codec.pgaRightNonInvSignalSelect(WM8960_PGAR_RINPUT2); 
+
+  // Connect from MIC inputs (aka pga output) to boost mixers
+  codec.connectLMIC2B();
+  codec.connectRMIC2B();
+
+  // Enable boost mixers
+  codec.enableAINL();
+  codec.enableAINR();
+
+  // Connect LB2LO (booster to output mixer (analog bypass)
+  codec.enableLB2LO();
+  codec.enableRB2RO();
+
+  // Disconnect from DAC outputs to output mixer
+  codec.disableLD2LO();
+  codec.disableRD2RO();
+
+  // Set gainstage between booster mixer and output mixer
+  codec.setLB2LOVOL(WM8960_OUTPUT_MIXER_GAIN_0DB); 
+  codec.setRB2ROVOL(WM8960_OUTPUT_MIXER_GAIN_0DB); 
+
+  // Enable output mixers
+  codec.enableLOMIX();
+  codec.enableROMIX();
+
+  // CLOCK STUFF, These settings will get you 44.1KHz sample rate, and class-d 
+  // freq at 705.6kHz
+  codec.enablePLL(); // Needed for class-d amp clock
+  codec.setPLLPRESCALE(WM8960_PLLPRESCALE_DIV_2);
+  codec.setSMD(WM8960_PLL_MODE_FRACTIONAL);
+  codec.setCLKSEL(WM8960_CLKSEL_PLL);
+  codec.setSYSCLKDIV(WM8960_SYSCLK_DIV_BY_2);
+  codec.setBCLKDIV(4);
+  codec.setDCLKDIV(WM8960_DCLKDIV_16);
+  codec.setPLLN(7);
+  codec.setPLLK(0x86, 0xC2, 0x26); // PLLK=86C226h
+  //codec.setADCDIV(0); // Default is 000 (what we need for 44.1KHz)
+  //codec.setDACDIV(0); // Default is 000 (what we need for 44.1KHz)
+  codec.setWL(WM8960_WL_16BIT);
+
+  codec.enablePeripheralMode();
+  //codec.enableMasterMode();
+  //codec.setALRCGPIO(); // Note, should not be changed while ADC is enabled.
+
+  // Enable ADCs, and disable DACs
+  codec.enableAdcLeft();
+  codec.enableAdcRight();
+  codec.disableDacLeft();
+  codec.disableDacRight();
+  codec.disableDacMute();
+
+  //codec.enableLoopBack(); // Loopback sends ADC data directly into DAC
+  codec.disableLoopBack();
+
+  // Default is "soft mute" on, so we must disable mute to make channels active
+  codec.enableDacMute(); 
+
+  codec.enableHeadphones();
+  codec.enableOUT3MIX(); // Provides VMID as buffer for headphone ground
+
+  codec.setHeadphoneVolumeDB(6.00);
+}
+
+void i2s_install() {
+  // Set up I2S Processor configuration
+  const i2s_config_t i2s_config = {
+    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 44100,
+    .bits_per_sample = i2s_bits_per_sample_t(16),
+    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 8,
+    .dma_buf_len = bufferLen,
+    .use_apll = false
+  };
+
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+}
+
+void i2s_setpin() {
+  // Set I2S pin configuration
+  const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = I2S_SDO,
+    .data_in_num = I2S_SD
+  };
+
+  i2s_set_pin(I2S_PORT, &pin_config);
+}
